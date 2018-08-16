c1a6c235324218a380f3cc4bcd984ca3c85b86c1
Lawrence <blandys@qq.com>
2018-08-16 18:28:16 +0800

    add some fiel

---
+++ playground/explore_overfitting_and_underfitting.py
@@ -0,0 +1,175 @@
+import os
+import shutil
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+NUM_WORDS = 1000
+
+cache_path = '/root/.keras/datasets'
+if os.path.exists(cache_path) == False:
+  shutil.copytree('./datasets', cache_path )
+
+(train_data, train_labels), (test_data, test_labels) = keras.datasets.imdb.load_data(num_words=NUM_WORDS)
+
+def multi_hot_sequences(sequences, dimension):
+    results = np.zeros((len(sequences), dimension))
+    for i, word_indices in enumerate(sequences):
+        results[i, word_indices] = 1.0
+    return results
+
+train_data = multi_hot_sequences(train_data, dimension=NUM_WORDS)
+test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
+
+plt.plot(train_data[0])
+
+baseline_model = keras.Sequential([
+    keras.layers.Dense(16, activation=tf.nn.relu, input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(16, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+baseline_model.compile(optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy', 'binary_crossentropy'])
+
+baseline_model.summary()
+
+baseline_history = baseline_model.fit(train_data,
+    train_labels,
+    epochs=20,
+    batch_size=512,
+    validation_data=(test_data, test_labels),
+    verbose=2)
+
+smaller_model = keras.Sequential([
+    keras.layers.Dense(4, activation=tf.nn.relu, input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(4, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+smaller_model.compile(optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy', 'binary_crossentropy'])
+
+smaller_model.summary()
+
+smaller_history = smaller_model.fit(train_data,
+    train_labels,
+    epochs=20,
+    batch_size=512,
+    validation_data=(test_data, test_labels),
+    verbose=2)
+
+bigger_model = keras.models.Sequential([
+    keras.layers.Dense(512, activation=tf.nn.relu, input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(512, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+bigger_model.compile(optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy', 'binary_crossentropy'])
+
+bigger_model.summary()
+
+bigger_history = bigger_model.fit(train_data, 
+    train_labels,
+    epochs=20,
+    batch_size=512,
+    validation_data=(test_data, test_labels),
+    verbose=2)
+
+imgIdx = 0
+
+def plot_history(histories, key='binary_crossentropy'):
+    plt.figure(figsize=(16, 10))
+
+    for name, history in histories:
+        val = plt.plot(history.epoch, history.history['val_' + key],
+            '--', label=name.title() + ' Val')
+        plt.plot(history.epoch, 
+            history.history[key], 
+            color=val[0].get_color(),
+            label=name.title() + ' Train')
+
+    
+    plt.xlabel('Epochs')
+    plt.ylabel(key.replace('_', ' ').title())
+    plt.legend()
+
+    plt.xlim([0, max(history.epoch)])
+    global imgIdx
+    plt.savefig('img{0}.png'.format(++imgIdx))
+
+plot_history([
+    ('baseline', baseline_history),
+    ('smaller', smaller_history),
+    ('bigger', bigger_history),
+])
+
+l2_model = keras.models.Sequential([
+    keras.layers.Dense(16, 
+        kernel_regularizer=keras.regularizers.l2(0.001),
+        activation=tf.nn.relu,
+        input_shape=(NUM_WORDS, )
+    ),
+    keras.layers.Dense(16, 
+        kernel_regularizer=keras.regularizers.l2(0.001),
+        activation=tf.nn.relu
+    ),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+l2_model.compile(optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy', 'binary_crossentropy'])
+
+l2_model_hisotry = l2_model.fit(
+    train_data, train_labels,
+    epochs=20,
+    batch_size=512,
+    validation_data=(test_data, test_labels),
+    verbose=2
+)
+
+plot_history([
+    ('baseline', baseline_history),
+    ('l2', l2_model_hisotry)
+])
+
+
+dpt_model = keras.models.Sequential([
+    keras.layers.Dense(
+        16, 
+        activation=tf.nn.relu,
+        input_shape=(NUM_WORDS, )
+    ),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(16, activation=tf.nn.relu),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+dpt_model.compile(optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy', 'binary_crossentropy'])
+
+dpt_model_history = dpt_model.fit(train_data, train_labels,
+    epochs=20,
+    batch_size=512,
+    validation_data=(test_data, test_labels),
+    verbose=2)
+
+plot_history([
+    ('baseline', baseline_history),
+    ('dropout', dpt_model_history)
+])
